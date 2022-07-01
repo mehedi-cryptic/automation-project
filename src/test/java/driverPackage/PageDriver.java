1bccb0e70b74b1db199d7d343311b1f3ba1db924
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-07-02 00:26:00 +0600

    first commit

---
+++ src/test/java/driverPackage/PageDriver.java
@@ -0,0 +1,27 @@
+package driverPackage;
+
+public class PageDriver {
+    private static final ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
+    private static PageDriver instance = null;
+
+    private PageDriver() {
+
+    }
+    public static PageDriver getInstance() {
+        if (instance == null) {
+            instance = new PageDriver();
+        }
+        return instance;
+    }
+
+
+    public WebDriver getDriver() {
+        return webDriver.get();
+    }
+    public void setDriver(WebDriver driver) {
+        webDriver.set(driver);
+    }
+    public static WebDriver getCurrentDriver() {
+        return getInstance().getDriver();
+    }
+}
